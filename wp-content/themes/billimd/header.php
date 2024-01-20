4b8a14e8f9b66cb8471a19f5c07bb096e1b30900
Stas <karamansky@gmail.com>
2024-01-20 13:02:14 +0200

    Added fixes

--- wp-content/themes/billimd/header.php
+++ wp-content/themes/billimd/header.php
@@ -128,7 +128,16 @@
 				</div>
 				<div class="header__hidden-support">
 					<span>Support</span>
-					<a href="tel:18772455163"><i class="icon clock-icon"></i>1 (877) 245-5163</a>
+					<a href="tel:18772455163">
+						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M20.7358 5.537V7.537H18.7358H20.7358" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+							<path d="M7.58691 10.729C7.58691 9.91799 8.24491 9.25999 9.05591 9.25999C9.86691 9.25999 10.5249 9.91799 10.5249 10.729C10.5249 12.565 7.58791 12.565 7.58791 14.739H10.5249" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+							<path d="M15.894 12.03L13.382 12.003C13.24 12.001 13.125 11.886 13.125 11.743V9.261" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+							<path d="M15.8901 9.32001V14.74" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+							<path d="M20.3 7.53801C18.697 4.63601 15.582 2.66701 12 2.66701C6.84499 2.66701 2.66699 6.84601 2.66699 12C2.66699 17.154 6.84499 21.333 12 21.333C17.155 21.333 21.333 17.154 21.333 12" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
+						1 (877) 245-5163
+					</a>
 				</div>
 			</div>
 	
@@ -773,7 +782,16 @@
 				</div>
 				<div class="footer__bottom-right">
 					<div class="footer__support">
-						<a href="tel:18772455163"><i class="icon clock-icon"></i>1 (877) 245-5163</a>
+						<a href="tel:18772455163">
+							<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M20.7358 5.537V7.537H18.7358H20.7358" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+								<path d="M7.58691 10.729C7.58691 9.91799 8.24491 9.25999 9.05591 9.25999C9.86691 9.25999 10.5249 9.91799 10.5249 10.729C10.5249 12.565 7.58791 12.565 7.58791 14.739H10.5249" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+								<path d="M15.894 12.03L13.382 12.003C13.24 12.001 13.125 11.886 13.125 11.743V9.261" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+								<path d="M15.8901 9.32001V14.74" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+								<path d="M20.3 7.53801C18.697 4.63601 15.582 2.66701 12 2.66701C6.84499 2.66701 2.66699 6.84601 2.66699 12C2.66699 17.154 6.84499 21.333 12 21.333C17.155 21.333 21.333 17.154 21.333 12" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+							</svg>
+							1 (877) 245-5163
+						</a>
 					</div>
 					<div class="footer__policy-menu--mob">
 						<ul>
