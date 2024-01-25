05514a0ea364a57866f95a5f11eb05206271b4e2
Stas <karamansky@gmail.com>
2024-01-25 12:33:18 +0200

    Added page-hero1 block

---
+++ wp-content/themes/billimd/template-pages/patient-eligibility-template.php
@@ -0,0 +1,26 @@
+<?php
+	/*
+	 * Template Name: Patient Eligibility template
+	 */
+	get_header();
+?>
+
+<section class="page-hero1">
+	<div class="wrapper">
+		<div class="page-hero1__inner">
+			<div class="page-hero1__left">
+				<h1 class="page-hero1__title">Patient Eligibility Verification</h1>
+				<a href="<?php echo get_home_url(); ?>/#contact-us" class="btn btn--big btn--green page-hero1__button">Contact Us Now</a>
+			</div>
+			<div class="page-hero1__right">
+				<div class="page-hero1__content">
+					<h2>Navigating patient eligibility can be complex and time-consuming, but it's crucial for efficient patient care and billing. That's where we come in.</h2>
+					<p>Our Patient Eligibility Verification service is designed to take the guesswork out of your billing process. We diligently verify insurance coverage, benefits, co-pays, and deductibles in real time, ensuring that you have accurate and up-to-date information before services are rendered.</p>
+					<p>This not only streamlines your billing cycle but also enhances patient satisfaction by preventing unexpected costs and delays. Don’t let eligibility uncertainties disrupt your practice. Call us today to secure a smooth, reliable billing process that supports your commitment to excellent patient care.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
