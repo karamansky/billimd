b83a39b47fddd0cc75888d98b83ff398ade13632
Stas <karamansky@gmail.com>
2024-01-25 11:54:30 +0200

    Separation code to header footer and frontpage templates

--- wp-content/themes/billimd/footer.php
+++ wp-content/themes/billimd/footer.php
@@ -1,3 +1,90 @@
+<footer class="footer">
+	<div class="wrapper">
+		<div class="footer__inner">
+			<div class="footer__top">
+				<div class="footer__menu">
+					<ul>
+						<li class="menu-item menu-item-has-children">
+							<a href="#">Solutions</a>
+							<ul class="sub-menu">
+								<li class="menu-item"><a href="/patient-eligibility-verification">Patient Eligibility Verification</a></li>
+								<li class="menu-item"><a href="#">Denial Management</a></li>
+								<li class="menu-item"><a href="#">Account Receivable (AR)</a></li>
+								<li class="menu-item"><a href="#">Coding Management</a></li>
+								<li class="menu-item"><a href="#">Medical Billing</a></li>
+								<li class="menu-item"><a href="#">Insurance Credentialing</a></li>
+							</ul>
+						</li>
+						<li class="menu-item"><a href="#">Who we Serve</a></li>
+						<li class="menu-item"><a href="#">Inside Look</a></li>
+						<li class="menu-item"><a href="#testimonials">Testimonials</a></li>
+						<li class="menu-item"><a href="#blog">News & blog</a></li>
+						<li class="menu-item"><a href="#contact-us">Message Our Team</a></li>
+					</ul>
+				</div>
+				<div class="footer__certifications">
+					<span>Certifications</span>
+					<div class="footer__certifications-items">
+						<div class="footer__certifications-item">
+							<img src="<?php echo get_template_directory_uri() ?>/assets/img/cert/hipaa.png" alt="cert" loading="lazy" />
+						</div>
+						<div class="footer__certifications-item">
+							<img src="<?php echo get_template_directory_uri() ?>/assets/img/cert/hl7.png" alt="cert" loading="lazy" />
+						</div>
+						<div class="footer__certifications-item">
+							<img src="<?php echo get_template_directory_uri() ?>/assets/img/cert/aapc.png" alt="cert" loading="lazy" />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footer__bottom">
+				<div class="footer__bottom-left">
+					<div class="footer__policy-menu">
+						<ul>
+							<li class="menu-item"><a href="#">Terms of Service</a></li>
+							<li class="menu-item"><a href="#">Privacy Policy</a></li>
+						</ul>
+					</div>
+					<div class="footer__copy">
+						Copyright © <?php echo date('Y'); ?> billiMD.com. All rights reserved.
+					</div>
+				</div>
+				<div class="footer__bottom-right">
+					<div class="footer__support">
+						<a href="tel:18772455163">
+							<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M20.7358 5.537V7.537H18.7358H20.7358" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+								<path d="M7.58691 10.729C7.58691 9.91799 8.24491 9.25999 9.05591 9.25999C9.86691 9.25999 10.5249 9.91799 10.5249 10.729C10.5249 12.565 7.58791 12.565 7.58791 14.739H10.5249" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+								<path d="M15.894 12.03L13.382 12.003C13.24 12.001 13.125 11.886 13.125 11.743V9.261" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+								<path d="M15.8901 9.32001V14.74" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+								<path d="M20.3 7.53801C18.697 4.63601 15.582 2.66701 12 2.66701C6.84499 2.66701 2.66699 6.84601 2.66699 12C2.66699 17.154 6.84499 21.333 12 21.333C17.155 21.333 21.333 17.154 21.333 12" stroke="black" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
+							</svg>
+							1 (877) 245-5163
+						</a>
+					</div>
+					<div class="footer__policy-menu--mob">
+						<ul>
+							<li class="menu-item"><a href="#">Terms of Service</a></li>
+							<li class="menu-item"><a href="#">Privacy Policy</a></li>
+						</ul>
+					</div>
+					<div class="footer__social-menu">
+						<ul>
+							<!--							<li class="menu-item linkedin"><a href="https://linkedin.com"></a></li>-->
+							<li class="menu-item twitter"><a href="https://twitter.com/billimd_com" target="_blank"></a></li>
+							<li class="menu-item facebook"><a href="https://www.facebook.com/profile.php?id=61555645943335" target="_blank"></a></li>
+							<li class="menu-item instagram"><a href="https://www.instagram.com/social.billimd" target="_blank"></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="footer__bottom-copy">
+				Copyright © <?php echo date('Y'); ?> billiMD.com. All rights reserved.
+			</div>
+		</div>
+	</div>
+</footer>
+
 <a href="tel:18772455163" class="btn help-btn">
 	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
 		<path class="strk" d="M20.736 5.53699V7.53699H18.736H20.736" stroke="#1B9260" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"/>
