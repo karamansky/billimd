111b1b31e90ce7cbeb425f0572419c2ca300cb58
Stas <karamansky@gmail.com>
2024-02-10 14:38:11 +0200

    Added policy template

---
+++ wp-content/themes/billimd/template-pages/policy-template.php
@@ -0,0 +1,123 @@
+<?php
+	/**
+	 * Template Name: Page: Policy page
+	 */
+	
+	get_header();
+	
+?>
+
+<section class="policy">
+	<div class="wrapper">
+		<div class="policy__inner">
+			<div class="policy__content">
+				<h1 class="policy__title">Cookies Policy</h1>
+				<div class="policy__date">
+					<span>Last updated: </span>February 05, 2024
+				</div>
+				<div class="policy__text">
+					<p>This Cookies Policy explains what Cookies are and how We use them. You should read this policy so You can understand what type of cookies We use, or the information We collect using Cookies and how that information is used.</p>
+					<p>Cookies do not typically contain any information that personally identifies a user, but personal information that we store about You may be linked to the information stored in and obtained from Cookies. For further information on how We use, store and keep your personal data secure, see our Privacy Policy.</p>
+					<p>We do not store sensitive personal information, such as mailing addresses, account passwords, etc. in the Cookies We use.</p>
+					
+					<h2>Interpretation and Definitions</h2>
+					<h3>Interpretation</h3>
+					<p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
+					
+					<h3>Definitions</h3>
+					<h4><b>For the purposes of this Cookies Policy:</b></h4>
+					<ul>
+						<li>
+							<b>Company</b> (referred to as either "the Company", "We", "Us" or "Our" in this Cookies Policy) refers to SYOAK LLC, 4300 BISCAYNE BLVD MIAMII, FL 33137.
+						</li>
+						<li>
+							<b>Cookies</b> means small files that are placed on Your computer, mobile device or any other device by a website, containing details of your browsing history on that website among its many uses.
+						</li>
+						<li>
+							<b>Website</b> refers to Billimd.com, accessible from <a href="https://billimd.com/">https://billimd.com/</a>
+						</li>
+						<li>
+							<b>You</b> means the individual accessing or using the Website, or a company, or any legal entity on behalf of which such individual is accessing or using the Website, as applicable.
+						</li>
+					</ul>
+					
+					<h2>The use of the Cookies</h2>
+					<h3>Type of Cookies We Use</h3>
+					<p>Cookies can be "Persistent" or "Session" Cookies. Persistent Cookies remain on your personal computer or mobile device when You go offline, while Session Cookies are deleted as soon as You close your web browser.</p>
+					<h4><b>We use both session and persistent Cookies for the purposes set out below:</b></h4>
+					<ul>
+						<li>
+							<span class="ul-title">Necessary / Essential Cookies</span>
+							<div><span>Type:</span>Session Cookies</div>
+							<div><span>Administered by:</span>Us</div>
+							<div><span>Purpose:</span>These Cookies are essential to provide You with services available through the Website and to enable You to use some of its features. They help to authenticate users and prevent fraudulent use of user accounts. Without these Cookies, the services that You have asked for cannot be provided, and We only use these Cookies to provide You with those services.</div>
+						</li>
+						<li>
+							<span class="ul-title">Cookies Policy / Notice Acceptance Cookies</span>
+							<div><span>Type:</span>Persistent Cookies</div>
+							<div><span>Administered by:</span>Us</div>
+							<div><span>Purpose:</span>These Cookies identify if users have accepted the use of cookies on the Website.</div>
+						</li>
+						<li>
+							<span class="ul-title">Functionality Cookies</span>
+							<div><span>Type:</span>Persistent Cookies</div>
+							<div><span>Administered by:</span>Us</div>
+							<div><span>Purpose:</span>These Cookies allow us to remember choices You make when You use the Website, such as remembering your login details or language preference. The purpose of these Cookies is to provide You with a more personal experience and to avoid You having to re-enter your preferences every time You use the Website.</div>
+						</li>
+						<li>
+							<span class="ul-title">Tracking and Performance Cookies</span>
+							<div><span>Type:</span>Persistent Cookies</div>
+							<div><span>Administered by:</span>Third-Parties</div>
+							<div><span>Purpose:</span>These Cookies are used to track information about traffic to the Website and how users use the Website. The information gathered via these Cookies may directly or indirectly identify you as an individual visitor. This is because the information collected is typically linked to a pseudonymous identifier associated with the device you use to access the Website. We may also use these Cookies to test new advertisements, pages, features or new functionality of the Website to see how our users react to them.</div>
+						</li>
+						<li>
+							<span class="ul-title">Targeting and Advertising Cookies</span>
+							<div><span>Type:</span>Persistent Cookies</div>
+							<div><span>Administered by:</span>Third-Parties</div>
+							<div><span>Purpose:</span>These Cookies track your browsing habits to enable Us to show advertising which is more likely to be of interest to You. These Cookies use information about your browsing history to group You with other users who have similar interests. Based on that information, and with Our permission, third party advertisers can place Cookies to enable them to show adverts which We think will be relevant to your interests while You are on third party websites.</div>
+						</li>
+						<li>
+							<span class="ul-title">Social Media Cookies</span>
+							<div><span>Type:</span>Persistent Cookies</div>
+							<div><span>Administered by:</span>Third-Parties</div>
+							<div><span>Purpose:</span>In addition to Our own Cookies, We may also use various third parties Cookies to report usage statistics of the Website, deliver advertisements on and through the Website, and so on. These Cookies may be used when You share information using a social media networking website such as Facebook, Instagram, Twitter or Google+.</div>
+						</li>
+					</ul>
+					
+					<h3>Your Choices Regarding Cookies</h3>
+					<p>If You prefer to avoid the use of Cookies on the Website, first You must disable the use of Cookies in your browser and then delete the Cookies saved in your browser associated with this website. You may use this option for preventing the use of Cookies at any time.</p>
+					<p>If You do not accept Our Cookies, You may experience some inconvenience in your use of the Website and some features may not function properly.</p>
+					<h4>If You'd like to delete Cookies or instruct your web browser to delete or refuse Cookies, please visit the help pages of your web browser.</h4>
+					<ul>
+						<li>
+							For the Chrome web browser, please visit this page from Google: <a href="//support.google.com/accounts/answer/32050">https://support.google.com/accounts/answer/32050</a>
+						</li>
+						<li>
+							For the Internet Explorer web browser, please visit this page from Microsoft: <a href="//support.microsoft.com/kb/278835">https://support.microsoft.com/kb/278835</a>
+						</li>
+						<li>
+							For the Firefox web browser, please visit this page from Mozilla: <a href="//support.mozilla.org/en-US/kb/delete-cookies-remove-info-websites-stored">https://support.mozilla.org/en-US/kb/delete-cookies-remove-info-websites-stored</a>
+						</li>
+						<li>
+							For the Safari web browser, please visit this page from Apple: <a href="//support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac">https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac</a>
+						</li>
+					</ul>
+					<p>For any other web browser, please visit your web browser's official web pages.</p>
+					
+					<h3>More Information about Cookies</h3>
+					<p>You can learn more about cookies: <a href="https://www.privacypolicies.com/blog/cookies/">What Are Cookies?</a></p>
+					
+					<h3>Contact Us</h3>
+					<h4>If you have any questions about this Cookies Policy, You can contact us:</h4>
+					<ul>
+						<li>By email: <a href="mailto:info@billimd.com">info@billimd.com</a></li>
+						<li>By visiting this page on our website: <a href="https://billimd.com/">https://billimd.com/</a></li>
+						<li>By phone number: <a href="tel:18772455163">+18772455163</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
