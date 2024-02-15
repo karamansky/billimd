d446f8f494d778e729009b45a67d3e991b86628a
Stas <karamansky@gmail.com>
2024-02-15 12:37:26 +0200

    added AR Service page link

---
+++ wp-content/themes/billimd/template-pages/ar-services-template.php
@@ -0,0 +1,25 @@
+<?php
+	/*
+	 * Template Name: Page: AR Services template
+	 */
+	get_header();
+?>
+<section class="hero page-hero2">
+	<div class="wrapper">
+		<div class="page-hero2__inner">
+			<h1 class="page-hero2__title">Account Receivable (AR) Services </h1>
+			<h2 class="page-hero2__subtitle">Revolutionize Your Financial Health with Our Expert AR Services</h2>
+			<div class="page-hero2__columns">
+				<div class="page-hero2__col">
+					<p>In today’s complex healthcare landscape, effective management of Accounts Receivable is crucial for the financial stability of your practice. Our AR Services are designed to address the challenges faced by healthcare providers, ensuring you receive the payments you rightfully deserve. With our comprehensive approach, we handle everything from claim submission to payment posting, and from denial management to aging AR cleanup.</p>
+				</div>
+				<div class="page-hero2__col">
+					<p>Our team of experts employs the latest techniques and technology to streamline your billing process, ensuring timely reimbursements and maximizing your revenue. We understand the importance of maintaining a healthy cash flow and are dedicated to reducing your days in AR, optimizing your revenue cycle, and enhancing patient satisfaction by alleviating billing hassles. Choose our AR Services for a more efficient, profitable, and stress-free financial management of your healthcare practice.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<?php get_footer(); ?>
