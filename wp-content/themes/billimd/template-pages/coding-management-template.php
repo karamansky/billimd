37ed9e2d842c83107080bb5359662df814b2a47c
Stas <karamansky@gmail.com>
2024-02-15 13:27:47 +0200

    Added cards--noimage block

---
+++ wp-content/themes/billimd/template-pages/coding-management-template.php
@@ -0,0 +1,161 @@
+<?php
+	/*
+	 * Template Name: Page: Coding Management template
+	 */
+	get_header();
+?>
+	<section class="text-columns" >
+		<div class="wrapper">
+			<div class="text-columns__inner">
+				<h2 class="text-columns__title">Coding Management (Medical Coding Services)</h2>
+				<div class="text-columns__block">
+					<div class="text-columns__col">
+						<h3>Unlock Precision in Medical Coding with Our Expert Support</h3>
+					</div>
+					<div class="text-columns__col"></div>
+				</div>
+				<div class="text-columns__block">
+					<div class="text-columns__col">
+						<p>In the dynamic world of healthcare, the precision and efficiency of medical coding are crucial for the financial sustainability of your practice. Our Coding Support service offers an unparalleled advantage. With expertise drawn from industry leaders, we provide meticulous coding services that are essential for accurate billing and reimbursement. Our certified coding specialists are well-versed in the latest ICD-10 updates and healthcare common procedure coding systems, ensuring every patient encounter is translated into universal alphanumeric codes accurately. This not only optimizes your revenue cycle but also significantly reduces the chances of claim denials.</p>
+					</div>
+					<div class="text-columns__col">
+						<p>Whether you're dealing with a backlog, staff shortage, or simply seeking to enhance the accuracy of your coding processes, our team seamlessly integrates into your workflow, delivering the support and expertise your practice deserves. By choosing our Coding Support service, you're not just outsourcing a task; you're investing in a partnership that understands and caters to your unique needs, ensuring a seamless, efficient, and cost-effective medical billing process. Embrace the opportunity to transform your medical practice with our Coding Support service, where accuracy meets efficiency.</p>
+					</div>
+				</div>
+				<div class="text-columns__block">
+					<div class="text-columns__col">
+						<h4>How crucial are medical coding services for the functioning of healthcare organizations?</h4>
+						<p>Medical coding is the process of translating healthcare services, diagnoses, and medical equipment into universal alphanumeric codes. This task requires a medical coder to review patient records, including doctors' notes, lab results, and lists of services rendered, to identify the correct codes for procedures and diagnoses. These codes are then used to craft a claim for submission to insurance payers, ensuring that all medical information from the documentation is accurately represented for billing purposes.</p>
+						<br>
+						<span>Medical coding services offered by <a href="billimd.com">Billimd.com</a></span>
+					</div>
+					<div class="text-columns__col">
+						<h4>Comprehensive Medical Coding in ICD-10, CPT, HCPCS, and DRG</h4>
+						<p>Expert services in diverse coding systems including International Classification of Diseases, Current Procedural Terminology, Healthcare Common Procedure Coding System, and Diagnosis-Related Group.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+	<section class="cards cards--noimage" data-aos="fade-up">
+		<div class="wrapper">
+			<div class="cards__inner">
+				<div class="cards__items">
+					<div class="cards__item">
+						<h3 class="cards__item-title">Comprehensive Medical Coding in ICD-10, CPT, HCPCS, and DRG:</h3>
+						<div class="cards__item-description">
+							Expert services in diverse coding systems including International Classification of Diseases, Current Procedural Terminology, Healthcare Common Procedure Coding System, and Diagnosis-Related Group.
+						</div>
+					</div>
+					<div class="cards__item">
+						<h3 class="cards__item-title">Risk Adjustment Data Validation Audit Services</h3>
+						<div class="cards__item-description">
+							Providing audits for validating risk adjustment data.
+						</div>
+					</div>
+					<div class="cards__item">
+						<h3 class="cards__item-title">Hierarchical Condition Category Coding Services</h3>
+						<div class="cards__item-description">
+							Specialized coding services focusing on the HCC model for risk adjustment.
+						</div>
+					</div>
+					<div class="cards__item">
+						<h3 class="cards__item-title">NCQA HEDIS Compliance and Coding Expertise</h3>
+						<div class="cards__item-description">
+							Specialized services ensuring compliance with National Committee for Quality Assurance Health Effectiveness Data and Information Set standards in medical coding.
+						</div>
+					</div>
+					<div class="cards__item">
+						<h3 class="cards__item-title">Medical Necessity Review and Auditing Services</h3>
+						<div class="cards__item-description">
+							Auditing services focused on the medical necessity of coded procedures and services.
+						</div>
+					</div>
+					<div class="cards__item">
+						<h3 class="cards__item-title">Surgical Coding Services</h3>
+						<div class="cards__item-description">
+							Expertise in coding for various surgical procedures.
+						</div>
+					</div>
+					<div class="cards__item">
+						<h3 class="cards__item-title">Focused Specialty Coding Services</h3>
+						<div class="cards__item-description">
+							Dedicated coding services tailored to specific medical specialties.
+						</div>
+					</div>
+					<div class="cards__item">
+						<h3 class="cards__item-title">Optimization of Risk Adjustment Factor Scores</h3>
+						<div class="cards__item-description">
+							Services aimed at enhancing RAF scores for better risk adjustment in medical billing.
+						</div>
+					</div>
+					<div class="cards__item">
+						<h3 class="cards__item-title">Compliance Audits Based on NCCI Protocols (VA/VE)</h3>
+						<div class="cards__item-description">
+							Conducting coding audits in line with National Correct Coding Initiative edits.
+						</div>
+					</div>
+					<div class="cards__item">
+						<h3 class="cards__item-title">Evaluation & Management Utilization Analysis</h3>
+						<div class="cards__item-description">
+							Benchmarking and analysis services for E&M coding levels.
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	
+	
+	<section class="cards" data-aos="fade-up">
+		<div class="wrapper">
+			<div class="cards__inner">
+				<h2 class="cards__title">What common obstacles do healthcare institutions encounter in medical coding?</h2>
+				<div class="cards__items">
+					<div class="cards__item">
+						<div class="cards__item-image">
+							<img src="<?php echo get_template_directory_uri() ?>/assets/img/cards/card9.svg" alt="card" loading="lazy" />
+						</div>
+						<h3 class="cards__item-title">Failure to use current & updated code sets</h3>
+					</div>
+					<div class="cards__item">
+						<div class="cards__item-image">
+							<img src="<?php echo get_template_directory_uri() ?>/assets/img/cards/card10.svg" alt="card" loading="lazy" />
+						</div>
+						<h3 class="cards__item-title">Poor or missing documentation</h3>
+					</div>
+					<div class="cards__item">
+						<div class="cards__item-image">
+							<img src="<?php echo get_template_directory_uri() ?>/assets/img/cards/card11.svg" alt="card" loading="lazy" />
+						</div>
+						<h3 class="cards__item-title">Lack of access to the provider</h3>
+					</div>
+					<div class="cards__item">
+						<div class="cards__item-image">
+							<img src="<?php echo get_template_directory_uri() ?>/assets/img/cards/card12.svg" alt="card" loading="lazy" />
+						</div>
+						<h3 class="cards__item-title">Failing to code at the highest level</h3>
+					</div>
+					<div class="cards__item">
+						<div class="cards__item-image">
+							<img src="<?php echo get_template_directory_uri() ?>/assets/img/cards/card13.svg" alt="card" loading="lazy" />
+						</div>
+						<h3 class="cards__item-title">Undercoding and overcoding</h3>
+					</div>
+					<div class="cards__item">
+						<div class="cards__item-image">
+							<img src="<?php echo get_template_directory_uri() ?>/assets/img/cards/card14.svg" alt="card" loading="lazy" />
+						</div>
+						<h3 class="cards__item-title">Unbundling of Codes</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+<?php get_footer(); ?>
